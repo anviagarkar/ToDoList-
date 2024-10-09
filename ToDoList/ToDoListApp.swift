a1289ef6a5b0cbe3f1a0e188664c35179d306e96
anviagarkar <139617645+anviagarkar@users.noreply.github.com>
2024-10-09 12:40:07 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Anvi Agarkar on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
