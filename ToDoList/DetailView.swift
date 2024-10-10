e2986793d5e065d061245b7eddd35ebe11f10f02
anviagarkar <139617645+anviagarkar@users.noreply.github.com>
2024-10-10 15:45:33 -0400

    first commit

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Anvi Agarkar on 09/10/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
